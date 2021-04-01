4e2c0674257d86d366d716b5be8447d12e2ca52e
Ying Zhou <yingzhou@thoughtworks.com>
2021-04-01 20:49:04 +0800

    [0000][Ying] refactor login logic

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser')
 const path = require('path');
-const axios = require("axios");
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
 const backendHost = process.env.API_ENDPOINT || 'http://localhost:8080';
@@ -11,12 +10,34 @@ const backendConfig = {
         'x-api-key': process.env.X_API_KEY || 'testApiKey',
     },
 }
+const sessionAge = 60 * 60 * 1000;
+const loginSkippedPaths = [
+    '/login',
+    '/static',
+    '/manifest.json',
+    '/favicon.ico',
+    '/logo',
+    '/api/authorize'
+];
+const isAuthorized = (session) => (!!session.username && session.expire > Date.now());
+const loginSkipped = (path) => (loginSkippedPaths.some((skippedPath) => (path.startsWith(skippedPath))));
+const simpleLogger = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] user: ${req.session.username || 'unauthorized'} ${req.method} ${req.path}`);
+    next();
+}
+const loginRequired = (req, res, next) => {
+    if (isAuthorized(req.session) || loginSkipped(req.path)) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
 
 const app = express();
 app.use(bodyParser());
 app.use(cookieSession({
     secret: process.env.COOKIE_SECRET || 'testCookieSecret',
-    maxAge: 60 * 60 * 1000,
+    maxAge: sessionAge,
     name: 'sipu-session',
     cookie: {
         secure: true,
@@ -24,46 +45,25 @@ app.use(cookieSession({
     },
 }));
 
-app.use((req, res, next) => {
-    console.log(`${new Date()} ${req.session.username || 'unauthorized'} ${req.method} ${req.path}`);
-    const skipLogin = [
-        '/login',
-        '/static',
-        '/manifest.json',
-        '/favicon.ico',
-        '/logo',
-        '/authorize'
-    ];
-    if (!!req.session.username || skipLogin.some((skipPath) => (req.path.startsWith(skipPath)))) {
-        next();
-    } else if (req.path === '/logout') {
-        req.session.destroy((error) => {
-            console.log(`logout failed: ${error}`)
-        })
-        res.redirect('/login');
-    } else {
-        res.redirect('/login');
+app.use(simpleLogger);
+app.use(loginRequired);
+
+app.post('/api/authorize', createProxyMiddleware({
+    target: backendHost,
+    ...backendConfig,
+    onProxyRes: (proxyRes, req, res) => {
+        req.session = {username: req.body.username, expire: Date.now() + sessionAge};
     }
-});
+}));
 
 app.use('/api', createProxyMiddleware({
     target: backendHost,
     ...backendConfig
 }));
 
-app.post('/authorize', (req, res) => {
-    return axios.post(`${backendHost}/api/authorize`, req.body, backendConfig).then(() => {
-        res.session = {username: req.body.username};
-        res.redirect('/home');
-    }).catch(() => {
-        res.status(403);
-        res.send('Invalid username or password')
-    });
-});
-
 app.use(express.static(path.join(__dirname, './build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './build/index.html'));
 });
-app.listen(process.env.PORT || 3000);
 
+app.listen(process.env.PORT || 3000);
