eaaa2eaf71e0fe2be1fdf6a0919396331b7d1f79
Ying Zhou <yingzhou@thoughtworks.com>
2021-04-01 21:26:03 +0800

    [0000][Ying] create simple login page

--- src/features/login/index.tsx
+++ src/features/login/index.tsx
@@ -1,6 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button, Form, Input, notification, Spin} from "antd";
+import {EyeInvisibleOutlined, EyeTwoTone} from '@ant-design/icons';
+import axios from "axios";
 
 const Login: React.FC = () => {
-    return <div>login form</div>
+    const [loading, setLoading] = useState(false);
+    const onFinish = (values: any) => {
+        setLoading(true);
+        axios.post('/api/authorize', values).then(() => {
+            window.location.assign('/home');
+        }).catch(() => {
+            notification.open({
+                message: 'Login Failed',
+                description: 'Invalid username or password',
+            });
+        }).finally(() => {
+            setLoading(false);
+        });
+    }
+    return (
+        <Spin spinning={loading}>
+            <Form
+                name="login"
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{required: true, message: 'Please input your username!'}]}
+                >
+                    <Input/>
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{required: true, message: 'Please input your password!'}]}
+                >
+                    <Input.Password
+                        iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
+                    />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Spin>
+    )
 }
 export default Login
