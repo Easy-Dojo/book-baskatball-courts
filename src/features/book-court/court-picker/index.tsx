701254843351f1f204b24e9e239421f95b6c2f8d
lrliang <392425595@qq.com>
2021-04-09 20:48:47 +0800

    [LLR] fix part of eslint

--- src/features/book-court/court-picker/index.tsx
+++ src/features/book-court/court-picker/index.tsx
@@ -1,63 +1,65 @@
-import {Empty, notification, Spin} from "antd";
-import React, {useEffect, useState} from "react";
-import {useTypedSelector} from "../../../app/hooks/useTypedSelector";
-import {selectCourts} from "../courtsSlice";
-import {CheckboxValueType} from "antd/lib/checkbox/Group";
-import ConfirmButton from "./ConfirmButton";
-import CourtContent from "./CourtContent";
-import courtsService from "../service";
-import {useHistory} from "react-router-dom";
-import {Pages} from "../../../app/routes";
+import { Empty, notification, Spin } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { CheckboxValueType } from 'antd/lib/checkbox/Group';
+import { useHistory } from 'react-router-dom';
+import { useTypedSelector } from '../../../app/hooks/useTypedSelector';
+import { selectCourts } from '../courtsSlice';
+import ConfirmButton from './ConfirmButton';
+import CourtContent from './CourtContent';
+import courtsService from '../service';
 
 const CourtSelectionBoard: React.FC = () => {
-    const history = useHistory()
-    const {data, loading, error} = useTypedSelector(selectCourts)
-    const [selectedCourts, setSelectedCourts] = useState<Array<CheckboxValueType>>([]);
+  const history = useHistory();
+  const { data, loading, error } = useTypedSelector(selectCourts);
+  const [selectedCourts, setSelectedCourts] = useState<Array<CheckboxValueType>>([]);
 
+  const onSelectionChange = (checkedValue: Array<CheckboxValueType>) => {
+    setSelectedCourts(checkedValue);
+  };
 
-    const onSelectionChange = (checkedValue: Array<CheckboxValueType>) => {
-        setSelectedCourts(checkedValue);
+  const onSubmit = async () => {
+    if (data) {
+      const { date, startTime, endTime } = data;
+      const params = {
+        date,
+        startTime,
+        endTime,
+        selectedCourts,
+      };
+      const { orderId } = await courtsService.bookCourts(params).catch((err) => {
+        notification.error({
+          message: '定场失败',
+          description: err.message,
+        });
+      });
+
+      history.push(`/order-confirmation/${orderId}`);
     }
+  };
 
-    const onSubmit = async () => {
-        if (data) {
-            const {date, startTime, endTime} = data;
-            const params = {
-                date,
-                startTime,
-                endTime,
-                selectedCourts
-            }
-            const {orderId} = await courtsService.bookCourts(params).catch((err)=>{
-                notification.error({
-                    message: '定场失败',
-                    description: err.message,
-                });
-            })
-
-            history.push(`${Pages.OrderResult.path}/${orderId}`)
-        }
+  useEffect(() => {
+    if (error) {
+      notification.error({
+        message: '查询时间错误',
+        description: error,
+      });
     }
+  }, [error]);
 
-    useEffect(() => {
-        if (error) {
-            notification.error({
-                message: '查询时间错误',
-                description: error,
-            });
-        }
-    }, [error])
-
-    return <Spin spinning={loading}>
-        {
+  return (
+    <Spin spinning={loading}>
+      {
             data
-                ? <>
-                    <CourtContent courts={data.courts} onChange={onSelectionChange}/>
-                    <ConfirmButton onSubmit={onSubmit}/>
+              ? (
+                <>
+                  <CourtContent courts={data.courts} onChange={onSelectionChange} />
+                  <ConfirmButton onSubmit={onSubmit} />
                 </>
-                : <Empty/>
+              )
+              : <Empty />
         }
-    </Spin>;
-}
+    </Spin>
+  );
+};
 
-export default CourtSelectionBoard
+export default CourtSelectionBoard;
--- src/features/book-court/index.tsx
+++ src/features/book-court/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Header from "./Header";
-import TimeScheduler from "./time-scheduler";
-import CourtSelectionBoard from "./court-picker";
+import React from 'react';
+import Header from './Header';
+import TimeScheduler from './time-scheduler';
+import CourtSelectionBoard from './court-picker';
 
-const BookCourt: React.FC = () => {
-    return <div className="book-court-layout">
-        <Header/>
-        <TimeScheduler />
-        <CourtSelectionBoard />
-    </div>
-}
+const BookCourt: React.FC = () => (
+  <div className="book-court-layout">
+    <Header />
+    <TimeScheduler />
+    <CourtSelectionBoard />
+  </div>
+);
 
-export default BookCourt
+export default BookCourt;
