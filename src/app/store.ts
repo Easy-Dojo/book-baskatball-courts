d037b239fe64a772e2bec040616ada897db211e2
lrliang <392425595@qq.com>
2021-04-10 16:33:50 +0800

    [LLR] remove unused code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import messageReducer from '../features/hello/messageSlice';
 import authorizeReducer from '../features/login/authorizeSlice';
 import courtsReducer from '../features/book-court/courtsSlice';
 
 export const store = configureStore({
   reducer: {
     courts: courtsReducer,
-    message: messageReducer,
     loginState: authorizeReducer,
   },
 });
