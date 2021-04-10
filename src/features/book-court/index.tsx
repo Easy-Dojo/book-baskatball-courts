1cfe04f037f76e3cedfd2cde445462bc5f6effdb
lrliang <392425595@qq.com>
2021-04-10 13:54:39 +0800

    [LLR] rename courts-selector

--- src/features/book-court/index.tsx
+++ src/features/book-court/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import TimeScheduler from './time-scheduler';
-import CourtSelectionBoard from './court-picker';
+import CourtSelectionBoard from './courts-selector';
 
 const BookCourt: React.FC = () => (
   <div className="book-court-layout">
