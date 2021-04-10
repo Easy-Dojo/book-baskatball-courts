b65d8e9e4572059e356ab4a7c691d9226b161224
lrliang <392425595@qq.com>
2021-04-10 16:48:06 +0800

    [LLR] update http proxy config fix send request 503

--- server.js
+++ server.js
@@ -1,89 +1,98 @@
-const express = require('express');
-const cookieSession = require('cookie-session');
+const express = require('express')
+const cookieSession = require('cookie-session')
 const bodyParser = require('body-parser')
-const path = require('path');
-const {createProxyMiddleware} = require('http-proxy-middleware');
+const path = require('path')
+const {createProxyMiddleware} = require('http-proxy-middleware')
 
 const backendConfig = {
-    target: process.env.API_ENDPOINT || 'http://localhost:8080',
-    changeOrigin: true,
-    secure: false,
-    headers: {
-        'x-api-key': process.env.X_API_KEY || 'testApiKey',
-    },
+  target: process.env.API_ENDPOINT || 'http://localhost:8080',
+  changeOrigin: true,
+  secure: false,
+  headers: {
+    'x-api-key': process.env.X_API_KEY || 'testApiKey',
+  },
 }
-const sessionAge = 60 * 60 * 1000;
+const sessionAge = 60 * 60 * 1000
 const loginSkippedPaths = [
-    '/login',
-    '/static',
-    '/manifest.json',
-    '/favicon.ico',
-    '/logo',
-    '/api/authorize'
-];
-const isAuthorized = (session) => (!!session.username && session.expire > Date.now());
-const loginSkipped = (path) => (loginSkippedPaths.some((skippedPath) => (path.startsWith(skippedPath))));
+  '/login',
+  '/static',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo',
+  '/api/authorize',
+]
+const isAuthorized = (session) => (!!session.username && session.expire >
+  Date.now())
+const loginSkipped = (path) => (loginSkippedPaths.some(
+  (skippedPath) => (path.startsWith(skippedPath))))
 
 const simpleLogger = (req, res, next) => {
-    if (!loginSkipped(req.path)) {
-        console.log(`[${new Date().toISOString()}] user: ${req.session.username || 'unauthorized'} ${req.method} ${req.path}`);
-    }
-    next();
+  if (!loginSkipped(req.path)) {
+    console.log(`[${new Date().toISOString()}] user: ${req.session.username ||
+    'unauthorized'} ${req.method} ${req.path}`)
+  }
+  next()
 }
 const loginRequired = (req, res, next) => {
-    if (isAuthorized(req.session) || loginSkipped(req.path)) {
-        next();
-    } else {
-        res.redirect('/login');
-    }
+  if (isAuthorized(req.session) || loginSkipped(req.path)) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
 }
 
-const app = express();
+const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use(cookieSession({
-    secret: process.env.COOKIE_SECRET || 'testCookieSecret',
-    maxAge: sessionAge,
-    name: 'sipu-session',
-    cookie: {
-        secure: true,
-        httpOnly: true,
-    },
-}));
+  secret: process.env.COOKIE_SECRET || 'testCookieSecret',
+  maxAge: sessionAge,
+  name: 'sipu-session',
+  cookie: {
+    secure: true,
+    httpOnly: true,
+  },
+}))
 
-app.use(simpleLogger);
-app.use(loginRequired);
+app.use(simpleLogger)
+app.use(loginRequired)
+
+app.use('/api', createProxyMiddleware({
+  ...backendConfig,
+  onProxyReq: (proxyReq, req, res) => {
+    if (!req.body || !Object.keys(req.body).length) {
+      return
+    }
+    const contentType = proxyReq.getHeader('Content-Type')
+    const writeBody = (bodyData) => {
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData))
+      proxyReq.write(bodyData)
+    }
+
+    if (contentType.includes('application/json') ||
+      contentType.includes('application/x-www-form-urlencoded')) {
+      writeBody(JSON.stringify(req.body))
+    }
+  },
+}))
 
 app.post('/api/authorize', createProxyMiddleware({
-    ...backendConfig,
-    onProxyRes: (proxyRes, req, res) => {
-        if (proxyRes.statusCode === 200) {
-            req.session = {username: req.body.username, expire: Date.now() + sessionAge};
-        }
-    },
-    onProxyReq: (proxyReq, req, res) => {
-        if (!req.body || !Object.keys(req.body).length) {
-            return;
-        }
-        const contentType = proxyReq.getHeader('Content-Type');
-        const writeBody = (bodyData) => {
-            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-            proxyReq.write(bodyData);
-        };
-
-        if (contentType.includes('application/json') || contentType.includes('application/x-www-form-urlencoded')) {
-            writeBody(JSON.stringify(req.body));
-        }
+  ...backendConfig,
+  onProxyRes: (proxyRes, req, res) => {
+    if (proxyRes.statusCode === 200) {
+      req.session = {
+        username: req.body.username,
+        expire: Date.now() + sessionAge,
+      }
     }
-}));
+  },
+}))
 
-app.use('/api', createProxyMiddleware(backendConfig));
-
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './build/index.html'));
-});
+  res.sendFile(path.join(__dirname, './build/index.html'))
+})
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
